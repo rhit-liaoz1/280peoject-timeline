03b76f1bbc26a38ee69c2906327b61f6b0fba7a3
L7907661 <hlbtaioe@163.com>
2021-01-24 10:01:21 -0500

    after changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,307 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
 
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.TimelineListController = class {
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
+	constructor(){
 
 	}
 
-	methodName() {
+	updateView(){
+
+	}
+
+	toggleDescription(){
 
 	}
 }
 
-/* Main */
-/** function and class syntax examples */
-rhit.main = function () {
-	console.log("Ready");
-};
+rhit.TimelineListModel = class {
+
+	constructor(){
+
+		this._documentSnapshots;
+		this._ref;
+		this._unsubscribe;
+	}
+
+	beginListenting(changeListener){
+
+	}
+
+	stopListening(){
+
+	}
+
+	createTimeline(title, description, viewPermission, editPermission){
+
+	}
+
+	get length(){
+
+	}
+
+	getTimelineAtIndex(index){
+
+	}
+}
+
+rhit.Timeline = class {
+
+	constructor(title, description){
+
+		this.title = title;
+		this.description = description;
+	}
+}
+
+rhit.SingleTimelineController = class {
+
+	constructor(){
+
+	}
+
+	updateView(){
+
+	}
+
+	filterByYear(yearRange){
+
+	}
+
+	setZoomLevel(level){
+
+	}
+
+	toggleEventTitles(){
+
+	}
+}
+
+rhit.SingleTimelineModel = class {
+
+	constructor(){
+
+		this._documentSnapshots;
+		this._ref;
+		this._unsubscribe;
+	}
+
+	beginListenting(changeListener){
+
+	}
+
+	stopListening(){
+
+	}
+
+	deleteTimeline(){
+
+	}
+
+	updateTimeline(title, description, viewPermission, editPermission){
+
+	}
+
+	createEvent(year, title, imageURL, description){
+
+	}
+
+	getEventAtIndex(index){
+
+	}
+
+	get length(){
+
+	}
+}
+
+rhit.Event = class {
+
+	constructor(year, title){
+
+		this.year = year;
+		this.title = title;
+	}
+}
+
+rhit.EventPageController = class {
+
+	constructor(){
+
+	}
+
+	updateView(){
+
+	}
+}
+
+rhit.EventPageModel = class {
+
+	constructor(){
+
+		this._documentSnapshot;
+		this._ref;
+		this._unsubscribe;
+	}
+
+	beginListenting(changeListener){
+
+	}
+
+	stopListening(){
+
+	}
+
+	deleteEvent(){
+
+	}
+
+	updateEvent(year, title, imageURL, description, favoriteCount, pageContributors){
+
+	}
+
+	toggleFavorite(){
+
+	}
+
+	get year(){
+
+	}
+
+	get title(){
+
+	}
+
+	get imageURL(){
+
+	}
+
+	get description(){
+
+	}
+
+	get favoriteCount(){
+
+	}
+
+	get pageContributors(){
+
+	}
+}
+
+rhit.ProfilePageController = class {
+
+	constructor(){
+
+		this.pageState;
+	}
+
+	updateView(){
+
+	}
+}
+
+rhit.LoginPageModel = class {
+
+	constructor(){
+
+		this._documentSnapshot;
+		this._ref;
+		this._unsubscribe;
+	}
+
+	beginListenting(changeListener){
+
+	}
+
+	stopListening(){
+
+	}
+
+	delete(){
+
+	}
+
+	updateProfile(username, name, imageURL, age, location, favoriteEvents, createdEvents){
+
+	}
+
+	get username(){
+
+	}
+
+	get imageURL(){
+
+	}
+
+	get age(){
+
+	}
+
+	get location(){
+
+	}
+
+	get favoriteEvents(){
+
+	}
+
+	get createdEvents(){
+
+	}
+}
+
+
+rhit.LoginPageController = class {
+
+	constructor(){
+
+	}
+
+	updateView(){
+
+	}
+}
+
+rhit.LoginPageModel = class {
+
+	constructor(){
+
+		this._user;
+	}
+
+	createProfile(username, name, imageURL, age, location){
+
+	}
+
+	beginListenting(changeListener){
+
+	}
+
+	stopListening(){
+
+	}
+
+	signIn(){
+
+	}
+
+	signOut(){
+
+	}
+
+	isSignedIn(){
+
+	}
+
+	get uid(){
+
+	}
+}
+
+rhit.main = function(){
+
+}
 
 rhit.main();
